219be141b86643b95b9961337ffac6cef9dd1d54
AbdelliOussama <oussema.abdelli@iteam-univ.tn>
2026-03-28 12:48:53 +0100

    Update GetRegionsQueryHandler.cs

--- src/ANNUAIRECONGO.Application/Features/Geography/Queries/GetRegions/GetRegionsQueryHandler.cs
+++ src/ANNUAIRECONGO.Application/Features/Geography/Queries/GetRegions/GetRegionsQueryHandler.cs
@@ -5,15 +5,21 @@ using ANNUAIRECONGO.Domain.Common.Results;
 using ANNUAIRECONGO.Domain.Geography;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace ANNUAIRECONGO.Application.Features.Geography.Queries.GetRegions;
 
 public sealed class GetRegionsQueryHandler(
+    ILogger<GetRegionsQueryHandler> logger,
     IAppDbContext context) : IRequestHandler<GetRegionsQuery, Result<List<RegionDto>>>
 {
+    private readonly ILogger<GetRegionsQueryHandler> _logger = logger;
+    private readonly IAppDbContext _context = context;
+
+
     public async Task<Result<List<RegionDto>>> Handle(GetRegionsQuery request, CancellationToken cancellationToken)
     {
-        var regions = await context.Regions
+        var regions = await _context.Regions
             .AsNoTracking()
             .ToListAsync(cancellationToken);
 
