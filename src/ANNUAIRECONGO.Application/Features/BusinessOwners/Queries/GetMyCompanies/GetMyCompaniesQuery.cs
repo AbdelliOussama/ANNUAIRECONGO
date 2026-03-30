c458fa987e9f15d34bd90c0a3796ef1b6a5e3339
AbdelliOussama <oussema.abdelli@iteam-univ.tn>
2026-03-30 05:14:51 +0100

    Remove ICached Query from GetMyCompanies Query

--- src/ANNUAIRECONGO.Application/Features/BusinessOwners/Queries/GetMyCompanies/GetMyCompaniesQuery.cs
+++ src/ANNUAIRECONGO.Application/Features/BusinessOwners/Queries/GetMyCompanies/GetMyCompaniesQuery.cs
@@ -5,11 +5,4 @@ using MediatR;
 
 namespace ANNUAIRECONGO.Application.Features.BusinessOwners.Queries.GetMyCompanies;
 
-public sealed record GetMyCompaniesQuery : ICachedQuery<Result<List<CompanyDto>>>
-{
-    public string CacheKey =>"MyCompanies";
-
-    public string[] Tags => ["Company"];
-
-    public TimeSpan Expiration => TimeSpan.FromMinutes(5);
-}
+public sealed record GetMyCompaniesQuery : IRequest<Result<List<CompanyDto>>>;
